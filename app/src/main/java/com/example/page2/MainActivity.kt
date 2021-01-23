fc997daaa71537f75c9ed5b44af7a8c333be3bca
jayraj <jayraj1999@gmail.com>
2021-01-23 12:01:22 +0530

    Final, I think

--- app/src/main/java/com/example/page2/MainActivity.kt
+++ app/src/main/java/com/example/page2/MainActivity.kt
@@ -1,58 +1,37 @@
 package com.example.page2
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
-import com.smarteist.autoimageslider.IndicatorView.animation.type.IndicatorAnimationType
-import com.smarteist.autoimageslider.SliderAnimations
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var toggle:ActionBarDrawerToggle
 
-
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (toggle.onOptionsItemSelected(item)){
+        if (toggle.onOptionsItemSelected(item))
             return true
-        }
-
         return super.onOptionsItemSelected(item)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(myToolbar)
 
         setupImageSlider()
         setupNavViewDrawer()
     }
 
     fun addItemsToList(list : MutableList<SliderItem>){
-
-        list.add(SliderItem(
-            "https://wallpapercave.com/wp/wp3850825.jpg",
-            "",
-            3214)
-        )
-        list.add(SliderItem(
-            "https://wallpapercave.com/wp/wp6058934.jpg",
-            "",
-            3214)
-        )
-        list.add(SliderItem(
-            "https://wallpapercave.com/wp/wp4676567.jpg",
-            "",
-            3214)
-        )
-        list.add(SliderItem(
-            "https://people.sc.fsu.edu/~jburkardt/data/jpg/auburn_logo.jpg",
-            "",
-            3214)
-        )
-
+        list.add(SliderItem("https://wallpapercave.com/wp/wp3850825.jpg", "", 3214))
+        list.add(SliderItem("https://wallpapercave.com/wp/wp6058934.jpg", "", 3214))
+        list.add(SliderItem("https://wallpapercave.com/wp/wp4676567.jpg", "", 3214))
+        list.add(SliderItem("https://people.sc.fsu.edu/~jburkardt/data/jpg/auburn_logo.jpg", "", 3214))
     }
 
     fun setupNavViewDrawer(){
@@ -63,7 +42,8 @@ class MainActivity : AppCompatActivity() {
 
         navView.setNavigationItemSelectedListener {
             when(it.itemId){
-                R.id.nav_home -> Toast.makeText(this, "asdf", Toast.LENGTH_SHORT).show()
+                R.id.nav_home -> Toast.makeText(applicationContext, "asdf", Toast.LENGTH_SHORT).show()
+                else -> Toast.makeText(applicationContext , "452353" , Toast.LENGTH_LONG).show()
             }
             true
         }
@@ -78,4 +58,13 @@ class MainActivity : AppCompatActivity() {
         val x = SlideAdapter(list,applicationContext)
         imageSlider.setSliderAdapter(x)
     }
+
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START)
+        }
+        else
+        super.onBackPressed()
+    }
+
 }
