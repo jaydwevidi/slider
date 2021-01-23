cb90501f4dbefe0259550816cbd5630af137005f
jayraj <jayraj1999@gmail.com>
2021-01-23 12:03:14 +0530

    3rd

--- app/src/main/java/com/example/page2/MainActivity.kt
+++ app/src/main/java/com/example/page2/MainActivity.kt
@@ -21,7 +21,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         setSupportActionBar(myToolbar)
+        //<style name="AppTheme" parent="Theme.AppCompat.Light.NoActionBar">
+        //past this in styles file
 
         setupImageSlider()
         setupNavViewDrawer()
